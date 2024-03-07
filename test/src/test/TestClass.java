6afe4ee794bdd4e055bcec516039cba327f1a17b
sysadmin <sysadmin@NEWGEN-LPT-027.newgen-ent.local>
2024-03-07 19:52:15 +0530

    Switched to clild branch1

--- test/src/test/TestClass.java
+++ test/src/test/TestClass.java
@@ -5,6 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Syntax added");
 		System.out.println("Syntax 2");
+		System.out.println("Switched to clild branch1");
 
 	}
 
