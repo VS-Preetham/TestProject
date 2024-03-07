533b6a845b04553d6a7072b0971ee79576b0a151
sysadmin <sysadmin@NEWGEN-LPT-027.newgen-ent.local>
2024-03-07 19:57:27 +0530

    Switched to master branch

--- test/src/test/TestClass.java
+++ test/src/test/TestClass.java
@@ -5,7 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Syntax added");
 		System.out.println("Syntax 2");
-
+System.out.println("Switched master branch");
 	}
 
 }
